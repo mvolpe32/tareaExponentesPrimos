9c214eb295421867b5a3a34bcbcaefaf366c127d
Martin V <mvolpe32@gmail.com>
2020-07-06 17:20:26 -0300

    Tarea programacion

---
+++ src/com/company/Main.java
@@ -0,0 +1,59 @@
+package com.company;
+import sun.java2d.pipe.SpanShapeRenderer;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+import java.math.*;
+import java.text.SimpleDateFormat;
+
+public class Main {
+
+    static void fillMatriz(int[][] myArray){
+        int numero = 0;
+        for (int i = 0; i < 41; i++) {
+            myArray[i][0] = numero;
+            numero = numero + 1;
+        }
+        numero = 0;
+        for (int j = 0; j < 41; j++) {
+            myArray[0][j] = numero;
+            numero++;
+        }
+    }
+
+    static void printMatriz(int[][] myArray) {
+        for (int i = 0; i < 41; i++) {
+            for (int j = 0; j < 41; j++) {
+                System.out.print(myArray[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    static void cargarMatriz(int[][] myArray) {
+        int numero, contador = 0;
+        int i, j;
+        for (i = 2; i < 41; i++) {
+            numero = i;
+            for (j = 2; j < 41; j++) {
+                while ((numero % j == 0) && (numero != 1)) {
+                    contador++;
+                    numero = numero / j;
+                }
+                myArray[i][j] = contador;
+                contador = 0;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[][] miMatriz = new int[41][41];
+        fillMatriz(miMatriz);
+        cargarMatriz(miMatriz);
+        printMatriz(miMatriz);
+    }
+
+}
+
+
