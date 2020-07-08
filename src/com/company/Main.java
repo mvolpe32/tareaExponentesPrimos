075feed9ec83212f7ccb42e50b583ba350ad7d88
Martin V <mvolpe32@gmail.com>
2020-07-08 14:52:11 -0300

    Para los muchachos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,40 +2,39 @@ package com.company;
 import sun.java2d.pipe.SpanShapeRenderer;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
-
 import java.math.*;
 import java.text.SimpleDateFormat;
 
 public class Main {
 
-    static void fillMatriz(int[][] myArray, int rows, int col){
+    static void fillAxes(int[][] myArray){
         int numero = 0;
-        for (int i = 0; i < rows; i++) {
+        for (int i = 0; i < myArray.length; i++) {
             myArray[i][0] = numero;
             numero = numero + 1;
         }
         numero = 0;
-        for (int j = 0; j < col; j++) {
+        for (int j = 0; j < myArray[0].length; j++) {
             myArray[0][j] = numero;
             numero++;
         }
     }
 
-    static void printMatriz(int[][] myArray, int rows, int col) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows; j++) {
+    static void printMatriz(int[][] myArray) {
+        for (int i = 0; i < myArray.length; i++) {
+            for (int j = 0; j < myArray[i].length; j++) {
                 System.out.print(myArray[i][j] + " ");
             }
-            System.out.print("\n");
+            System.out.printf("\n");
         }
     }
 
-    static void cargarMatriz(int[][] myArray, int rows, int col) {
+    static void cargarMatriz(int[][] myArray) {
         int numero, contador = 0;
         int i, j;
-        for (i = 2; i < rows; i++) {
+        for (i = 2; i < myArray.length; i++) {
             numero = i;
-            for (j = 2; j < col; j++) {
+            for (j = 2; j < myArray[i].length; j++) {
                 while ((numero % j == 0) && (numero != 1)) {
                     contador++;
                     numero = numero / j;
@@ -51,9 +50,9 @@ public class Main {
         int rows= 101;
         int col = 101;
         int[][] miMatriz = new int[rows][col];
-        fillMatriz(miMatriz, rows, col);
-        cargarMatriz(miMatriz, rows, col);
-        printMatriz(miMatriz, rows, col);
+        fillAxes(miMatriz);
+        cargarMatriz(miMatriz);
+        printMatriz(miMatriz);
     }
 
 }
